54719bd767c4a73411f8c80958fd0ac6763cd512
Ryan <ryan@ryannd.com>
2023-05-24 22:28:57 -0500

    Added OrderDetail page and related infrastructure, completed Unit 5

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -19,12 +19,29 @@ public class OrdersController : ControllerBase
     public async Task<ActionResult<List<OrderWithStatus>>> GetOrders()
     {
         var orders = await _db.Orders
- 	    .Include(o => o.Pizzas).ThenInclude(p => p.Special)
- 	    .Include(o => o.Pizzas).ThenInclude(p => p.Toppings).ThenInclude(t => t.Topping)
- 	    .OrderByDescending(o => o.CreatedTime)
- 	    .ToListAsync();
+            .Include(o => o.Pizzas).ThenInclude(p => p.Special)
+            .Include(o => o.Pizzas).ThenInclude(p => p.Toppings).ThenInclude(t => t.Topping)
+            .OrderByDescending(o => o.CreatedTime)
+            .ToListAsync();
 
         return orders.Select(o => OrderWithStatus.FromOrder(o)).ToList();
+    }
+
+    [HttpGet("{orderId}")]
+    public async Task<ActionResult<OrderWithStatus>> GetOrder(int orderId) 
+    {
+        var order = await _db.Orders
+            .Where(o => o.OrderId == orderId)
+            .Include(o => o.Pizzas).ThenInclude(p => p.Special)
+            .Include(o => o.Pizzas).ThenInclude(p => p.Toppings).ThenInclude(t => t.Topping)
+            .SingleOrDefaultAsync();
+
+        if (order is null) 
+        {
+            return NotFound();
+        }
+
+        return OrderWithStatus.FromOrder(order);
     }
 
     [HttpPost]
